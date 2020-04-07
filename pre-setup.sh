dbcb0821c59f66e035d12c27fc676e2ab8c1d44c
Karol <sokoow@gmail.com>
2020-04-07 09:09:47 +0200

    Eoan test #1

--- pre-setup.sh
+++ pre-setup.sh
@@ -9,3 +9,9 @@ sudo pip install --upgrade pip
 
 sudo pip install setuptools
 sudo pip install ansible python-apt
+
+cd /tmp
+git clone https://github.com/sokoow/laptop-configs.git
+cd laptop-configs
+git checkout tags/0.3 -b eoan-0.1
+sudo ./laptop_setup.sh

11a3414948507e41ad04b1aba6a42e77c093a069
sokoow <sokoow@users.noreply.github.com>
2020-04-07 09:18:20 +0200

    Update pre-setup.sh

--- pre-setup.sh
+++ pre-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update --allow-releaseinfo-change
-sudo apt-get install -y mc git vim python-pip python apt-transport-https aptitude ntpdate software-properties-common ca-certificates curl build-essential python-apt
+sudo apt-get install -y mc git vim python-pip python apt-transport-https aptitude ntpdate software-properties-common ca-certificates curl build-essential python-apt python3-pip
 
 sudo ntpdate time.task.gda.pl
 
@@ -13,5 +13,5 @@ sudo pip install ansible python-apt
 cd /tmp
 git clone https://github.com/sokoow/laptop-configs.git
 cd laptop-configs
-git checkout tags/0.3 -b eoan-0.1
+git checkout tags/0.3.1 -b eoan-0.1
 sudo ./laptop_setup.sh
